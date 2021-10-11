c3edff904cf48902014abc7cc6934779aefdc83b
iamfaqeehhokyky <sirtunx.hokyky@gmail.com>
2021-10-11 01:07:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,87 @@
+from movie_theater import Theater
+import simpy
+import random
+import statistics
+
+wait_times = []
+theater = Theater
+
+
+def go_to_movies(env, moviegoer, theater):
+    # Moviegoer arrives at the theater
+    arrival_time = env.now
+
+    with theater.cashier.request() as request:
+        yield request
+        yield env.process(theater.purchase_ticket(moviegoer))
+
+    with theater.usher.request() as request:
+        yield request
+        yield env.process(theater.check_ticket(moviegoer))
+
+    if random.choice([True, False]):
+        with theater.server.request() as request:
+            yield request
+            yield env.process(theater.sell_food(moviegoer))
+
+    # Moviegoer heads into the theater
+    wait_times.append(env.now - arrival_time)
+
+
+def run_theater(env, num_cashiers, num_servers, num_ushers):
+    theater = Theater(env, num_cashiers, num_servers, num_ushers)
+
+    for moviegoer in range(3):
+        env.process(go_to_movies(env, moviegoer, theater))
+
+    while True:
+        yield env.timeout(0.20)  # Wait a bit before generating a new person
+
+        moviegoer += 1
+        env.process(go_to_movies(env, moviegoer, theater))
+
+
+def calculate_average_wait_time(wait_times):
+    average_wait = statistics.mean(wait_times)
+    # Pretty print the results
+    minutes, frac_minutes = divmod(average_wait, 1)
+    seconds = frac_minutes * 60
+    return round(minutes), round(seconds)
+
+
+def get_user_input():
+    num_cashiers = input("Input the number of working cashiers: ")
+    num_servers = input("Input the number of working servers: ")
+    num_ushers = input("Input the number of working ushers: ")
+    params = [num_cashiers, num_servers, num_ushers]
+    if all(str(i).isdigit() for i in params):  # Check input is valid
+        params = [int(x) for x in params]
+    else:
+        print(
+            "Could not parse input. The simulation will use default values:",
+            "\n1 cashier, 1 server, 1 usher.",
+        )
+        params = [1, 1, 1]
+    return params
+
+
+def main():
+    # Setup
+    random.seed(42)
+    num_cashiers, num_servers, num_ushers = get_user_input()
+
+    # Run the simulation
+    env = simpy.Environment()
+    env.process(run_theater(env, num_cashiers, num_servers, num_ushers))
+    env.run(until=90)
+
+    # View the results
+    mins, secs = calculate_average_wait_time(wait_times)
+    print(
+        "Running simulation...",
+        f"\nThe average wait time is {mins} minutes and {secs} seconds.",
+    )
+
+
+if __name__ == '__main__':
+    main()
